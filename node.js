e747a9be963b5bef48bb2f569c1a3b5c63a998a5
Patit <patit@parentheses.co.in>
2022-01-13 12:39:43 +0530

    <commit_message>

--- node.js
+++ node.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const app = express();
+
+function convertToBinary(x) {
+  let bin = 0;
+  let rem, i = 1, step = 1;
+  while (x != 0) {
+      rem = x % 2;
+      console.log(
+          `Step ${step++}: ${x}/2, Remainder = ${rem}, Quotient = ${parseInt(x/2)}`
+      );
+      x = parseInt(x / 2);
+      bin = bin + rem * i;
+      i = i * 10;
+  }
+  console.log(`Binary: ${bin}`);
+  return bin;
+}
  
+app.get('/binary/:number', (req, res) => {
+  res
+    .status(200)
+    .json(convertToBinary(req.params.number))
+    .end();
+});
+
 app.get('/', (req, res) => {
   res
     .status(200)
